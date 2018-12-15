8356fcd04ed94e15526bda0c7370b499d4b28ae1
Tyler Duckworth <s848387@stm5m-kcs29015.attlocal.net>
2018-12-15 09:55:06 -0500

    travis init commit

--- getjson.py
+++ getjson.py
@@ -4,35 +4,38 @@ http = urllib3.PoolManager()
 URL = 'http://www.thebluealliance.com/api/v3/events/2017'
 PARAMS = {'X-TBA-Auth-Key':  'lHqQTfGJ1B6WbSYAMLXRrjbB3ylOb26VnKWC7nNIeyZH7cobjTgQ1K43YDADulep'}
 file = open('output.txt', 'w')
+
+
 def get_tba():
     request = http.request('GET', URL, PARAMS)
     data = json.loads(request.data.decode('utf-8'))
     with open('output.txt', 'w') as outfile:
         for i in data:
-            if i['country'] =='USA':
-                    json.dump(
-                        {
-                            "type": "Feature",
-                            "properties": {
-                                "name": i['name'],
-                                "marker-symbol": "circle",
-                                "marker-size": "medium",
-                                "marker-color": "#357e7e",
-                                "key": i['key'],
-                                "address": i["address"],
-                                "city": i["city"],
-                                "state_prov": i["state_prov"]
-                            },
-                            "geometry": {
-                                "coordinates": [
+            if i['country'] == 'USA':
+                json.dump(
+                    {
+                        "type": "Feature",
+                        "properties": {
+                            "name": i['name'],
+                            "marker-symbol": "circle",
+                            "marker-size": "medium",
+                            "marker-color": "#357e7e",
+                            "key": i['key'],
+                            "address": i["address"],
+                            "city": i["city"],
+                            "state_prov": i["state_prov"]
+                        },
+                        "geometry": {
+                            "coordinates": [
                                 i["lng"],
                                 i["lat"]
-                                ],
-                                "type": "Point"
-                            },
-                        }, outfile, indent=1
-                    )
-            
-            
+                            ],
+                            "type": "Point"
+                        },
+                    }, outfile, indent=1
+                )
+
     print()
+
+
 get_tba()
